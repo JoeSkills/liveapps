bf909ed837f9c98c32b5f1b497ee9def6e85b935
EasyWeb24 <josephoritseweyinmi5@gmail.com>
2024-06-06 03:08:46 +0100

    Latest commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import MainNav from "@/components/MainNav";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <MainNav>{children}</MainNav>
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
